e56076849388ac9db27d3ff14ed5675587178cb3
Sayantan <sayantan.kundu95@gmail.com>
2020-07-26 19:24:52 +0530

    Decryption of password and token generation

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,19 +1,38 @@
 const HttpError = require('../models/error');
 const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 // Function to add users after clicking sign up button
 exports.postAddUser = (req, res, next) => {
     const { firstName, lastName, email, password } = req.body;
-    const user = new User({
-        userName: firstName + Math.random().toString(36).slice(7),  // autogenerating username
-        firstName: firstName,
-        lastName: lastName,
-        email: email,
-        password: password
-    });
-    user.save()
-        .then(result => {
-            res.json({ message: "Registered successfully." })
+    User.findOne({ email: email })
+        .then(async result => {
+            if (result) {
+                return next(new HttpError('Email is already registered', 409));
+            }
+            let hashedPassword;
+            try {
+                hashedPassword = await bcrypt.hash(password, 12);
+            } catch (err) {
+                console.log(err);
+            }
+
+            const user = new User({
+                userName: firstName + Math.random().toString(36).slice(7),  // autogenerating username
+                firstName: firstName,
+                lastName: lastName,
+                email: email,
+                password: hashedPassword
+            });
+            user.save()
+                .then(result => {
+                    res.json({ message: "Registered successfully." })
+                })
+                .catch(err => {
+                    return next(new HttpError('An unknown error occurred ! Please check after sometime...', 404));
+                });
+
         })
         .catch(err => {
             return next(new HttpError('An unknown error occurred ! Please check after sometime...', 404));
@@ -23,17 +42,55 @@ exports.postAddUser = (req, res, next) => {
 // Function to get authenticated user after clicking sign in button
 exports.getAuthUser = (req, res, next) => {
     const { email, password } = req.query;
-    console.log(email);
-    console.log(password);
 
-    User.findOne({ $and: [{ email: email }, { password: password }] })
-        .then(result => {
-            console.log(result);
-            res.json({ result });
+    User.findOne({ email: email })
+        .then(async result => {
+            if (result) {
+                let existingPassword = result.password;
+                let { userName, email, firstName, lastName, isAdmin, college, stream, course, phoneNo } = result;
+                let isValidPassword;
+                try {
+                    isValidPassword = await bcrypt.compare(password, existingPassword);
+                } catch (err) {
+                    console.log(err);
+                }
+                if (isValidPassword) {
+                    let token;
+                    try {
+                        console.log(process.env.JWT_TOKEN);
+                        token = jwt.sign(
+                            { userName: userName, email: email },
+                            process.env.JWT_TOKEN,
+                            { expiresIn: '1h' }
+                        );
+                    } catch (err) {
+                        return next(new HttpError('Could not log you in...Please try after sometime', 401));
+                    }
+
+                    res.json(
+                        {
+                            result:
+                            {
+                                userName: userName,
+                                email: email,
+                                firstName: firstName,
+                                isAdmin: isAdmin,
+                                college: college,
+                                stream: stream,
+                                course: course,
+                                phoneNo: phoneNo,
+                                token: token
+                            }
+                        });
+                } else {
+                    return next(new HttpError('Invalid password', 401));
+                }
+            } else {
+                return next(new HttpError('Email is not registered', 401));
+            }
         })
         .catch(err => {
-            console.log(err);
-            return next(new HttpError('Email is not registered', 401));
+            return next(new HttpError('An unknown error occurred ! Please check after sometime...', 500));
         });
 
 }
